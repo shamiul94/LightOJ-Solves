03adc38d5aca04d4b035d31583189dc825cd0dac
shamiul94 <1505038.sh@ugrad.cse.buet.ac.bd>
2018-10-21 10:49:05 +0600

    1283

--- 1049 - One Way Roads.cpp
+++ 1049 - One Way Roads.cpp
@@ -1,19 +1,19 @@
 /**
-@author - Rumman BUET CSE'15
-Problem - 
-Idea - 
-Concept - 
+*@author - Rumman BUET CSE'15
+*Problem -
+*Idea -
+*Concept -
 */
- 
+
 #include <bits/stdc++.h>
 
 #define ll                                      long long int
 #define ull                                     unsigned long long
 #define ld                                      long double
- 
+
 #define ff                                      first
 #define ss                                      second
- 
+
 #define fi                                      freopen("in.txt", "r", stdin)
 #define fo                                      freopen("out.txt", "w", stdout)
 #define m0(a) memset(a , 0 , sizeof(a))
@@ -23,35 +23,35 @@ Concept -
 #define what_is(x)                              cout << #x << " is " << x << endl
 #define pf                                      printf
 #define sf                                      scanf
- 
+
 #define pb                                      push_back
 #define mp                                      make_pair
 #define eb                                      emplace_back
 #define pii                                     pair<int, int>
 #define piii                                    pair<pii, int>
- 
+
 #define SQR(a)                                  ((a)*(a))
 #define QUBE(a)                                 ((a)*(a)*(a))
- 
+
 #define scanI(a)                                scanf("%d",&a)
 #define scanI2(a,b)                             scanI(a) , scanI(b)
 #define scanI3(a,b,c)                           scanI(a), scanI(b), scanI(c)
 #define scanI4(a,b,c,d)                         scanI(a), scanI(b), scanI(c), scanI(d)
- 
+
 #define sll(a)                                scanf("%lld",&a)
 #define sll2(a,b)                             sll(a) , sll(b)
 #define sll3(a,b,c)                           sll(a), sll(b), sll(c)
 #define sll4(a,b,c,d)                         sll(a), sll(b), sll(c), sll(d)
- 
+
 #define inf LLONG_MAX
 #define minf LLONG_MIN
 #define min3(a,b,c) min(a,min(b,c))
 #define max3(a,b,c) max(a,max(b,c))
 #define ones(mask)  __builtin_popcount(mask)
 #define mx 150000
- 
+
 using namespace std;
- 
+
 ll BigMod(ll B,ll P,ll M)
 {
     ll R=1;
@@ -66,7 +66,7 @@ ll BigMod(ll B,ll P,ll M)
     }
     return R;
 }
- 
+
 int Set(int N,int pos)
 {
     return N=N | (1<<pos);
@@ -79,18 +79,40 @@ bool check(int N,int pos)
 {
     return (bool)(N & (1<<pos));
 }
- 
+
 /************************************** END OF INITIALS ****************************************/
+
+ll N ;
+vector<ll> v[105];
+ll cost[105][105];
+
+ll dfs(ll node)
+{
+
+}
 
 int main()
 {
-    ll T , t = 0 ; 
-
-    sll(T); 
+    ll T, t = 0 ;
+    sll(T);
 
     while(T--)
     {
-        t++; 
+        t++;
+        sll(N);
+        ll a, b, c ;
+        for(ll i = 0 ; i < N ; i++)
+        {
+            sll3(a,b,c);
+            v[a].push_back(b);
+            v[b].push_back(a);
+            cost[b][a] = c ;
+            cost[a][b] = 0 ;
+        }
+        ll ans ;
+        ans = dfs();
+        printf("Case %lld: ",t);
+
     }
-    return 0; 
+    return 0;
 }
